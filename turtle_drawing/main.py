35b071c887d7db49fefb3f4a7e5bb21ce8187e07
Kelly Butler <butlerk@bhs.vic.edu.au>
2021-09-03 20:37:52 +1000

    changed the draw_shape function to objects

--- turtle_drawing/main.py
+++ turtle_drawing/main.py
@@ -1,7 +1,5 @@
 import turtle
-from drawing import Shape
-from drawing import Point
-
+import drawing
 
 def draw_shape(my_turtle, shape):
     ''' 
@@ -10,22 +8,25 @@ def draw_shape(my_turtle, shape):
     '''
     # TODO: This function should be written to be a method in the Shape class
         
-    points = shape['points'] # gets list of points to draw
+    points = shape.points # gets list of points to draw
     # Put the pen up, so the turtle isn't drawing on the canvas and move to first point
     my_turtle.penup()
-    my_turtle.goto(points[0])
+    my_turtle.goto((points[0].x,points[0].y))
+
 
     # Sets up colour for shape fill colour and puts pen down
-    my_turtle.color(shape['colour'])
+    my_turtle.color(shape.colour)
     my_turtle.begin_fill()
     my_turtle.pendown()
 
     # Moves around to different points to draw the shape
     for point in points:
-        my_turtle.goto(point)
+        #my_turtle.goto(point)
+        my_turtle.goto((point.x,point.y))
     
     # Moves to first point, to close the shape
-    my_turtle.goto(points[0])
+    #my_turtle.goto(points[0])
+    my_turtle.goto((points[0].x,points[0].y))
     my_turtle.end_fill()
 
 '''
